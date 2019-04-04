d3e2141ee9101f2bff701a2924d04a218b9b254a
Tushar Tambe <44019295+tushartambe@users.noreply.github.com>
2019-04-04 11:41:27 +0530

    css change of card display

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Game from './Game';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Game from "./Game";
 
 const handleDrag = function(event) {
-  event.dataTransfer.setData('text', event.target.id);
+  event.dataTransfer.setData("text", event.target.id);
 };
 
 const handleDrop = function(event) {
   event.preventDefault();
-  var data = event.dataTransfer.getData('text');
-  event.target.appendChild(document.getElementById(data));
+  let data = event.dataTransfer.getData("text");
+  let dropTartget = event.target;
+  let card = document.getElementById(data);
+  if (dropTartget.className == "drop-here") {
+    // card.className = "card-on-stack";
+    if (dropTartget.childNodes.length == 0) {
+      event.target.appendChild(card);
+      return;
+    }
+    event.target.replaceChild(card, dropTartget.childNodes[0]);
+  }
+
+  if (dropTartget.className == "piles") {
+    // let card = document.getElementById(data);
+    card.className = "card-on-piles";
+    event.target.appendChild(card);
+  }
 };
 
 const allowDrop = function(event) {
@@ -24,7 +39,7 @@ const Piles = function(props) {
       <div
         id={`piles_${i}`}
         key={`piles_${i}`}
-        className='piles'
+        className="piles"
         onDrop={handleDrop}
         onDragOver={allowDrop}
       />
@@ -41,7 +56,7 @@ const Stack = function(props) {
       <div
         id={`stack_${i}`}
         key={`stack_${i}`}
-        className='drop-here'
+        className="drop-here"
         onDrop={handleDrop}
         onDragOver={allowDrop}
       />
@@ -52,12 +67,14 @@ const Stack = function(props) {
 };
 
 const Card = function(card) {
-  let div = document.createElement('div');
+  let div = document.createElement("div");
   div.innerHTML = card.unicode;
-  div.className = 'card';
-  div.id = 'card_' + card.number;
+  div.className = "card";
+
+  div.id = "card_" + card.symbol.type + card.number;
+
   div.style.color = card.symbol.color;
-  div.setAttribute('draggable', true);
+  div.setAttribute("draggable", true);
   div.ondragstart = handleDrag;
   return div;
 };
@@ -74,7 +91,7 @@ class StartGame extends React.Component {
     let card = new Game().drawCardFromDeck();
     console.log(new Game().deck);
 
-    let divElelmet = document.getElementById('open-cards');
+    let divElelmet = document.getElementById("open-cards");
     let div = Card(card);
     divElelmet.appendChild(div);
   }
@@ -82,18 +99,18 @@ class StartGame extends React.Component {
   returnCards() {
     return (
       <div>
-        <div className='top-section'>
-          <div className='deck' onClick={this.displayCurrentCard}>
+        <div className="top-section">
+          <div className="deck" onClick={this.displayCurrentCard}>
             Deck
           </div>
-          <div className='open-cards' id='open-cards' />
-          <div className='place-here'>
+          <div className="open-cards" id="open-cards" />
+          <div className="place-here">
             <Stack size={4} />
           </div>
         </div>
         <hr />
         <div>
-          <div className='pile-lots'>
+          <div className="pile-lots">
             <Piles size={7} />
           </div>
         </div>
@@ -106,4 +123,4 @@ class StartGame extends React.Component {
   }
 }
 
-ReactDOM.render(<StartGame />, document.getElementById('root'));
+ReactDOM.render(<StartGame />, document.getElementById("root"));
